3b91101a74ae7a07d968f77d2b703dec2b66bfb2
Cleison Amorim <amorim.cleison@gmail.com>
2018-10-07 02:11:12 -0300

    Including download of face and hand models

--- tools/get_models.sh
+++ tools/get_models.sh
@@ -13,9 +13,9 @@ done < "$reference_model"
 # ------------------------- BODY, FACE AND HAND MODELS -------------------------
 # Downloading body pose (COCO and MPI), face and hand models
 OPENPOSE_URL="http://posefs1.perception.cs.cmu.edu/OpenPose/models/"
-POSE_FOLDER="pose/"
-FACE_FOLDER="face/"
-HAND_FOLDER="hand/"
+POSE_FOLDER="models/pose/"
+FACE_FOLDER="models/face/"
+HAND_FOLDER="models/hand/"
 
 # ------------------------- POSE MODELS -------------------------
 # Body (BODY_25)
